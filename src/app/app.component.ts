24e6c3d9f8717d5081b89d4dcd8e967ec53e8adb
JhonAcevedo02 <jhonacevedo@seti.com.co>
2022-07-01 10:57:41 -0500

    diseño, formularios, y validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HU-7';
+
+  aprendiz = 'Andrés García';
+  lider = 'Ana María Garzón';
+  fecha : Date = new Date;
+  
+  accion = new FormControl('');
+
+  actividad = new FormControl('', [Validators.required]);
+
+  responsable = new FormControl('neider');
+
+  estado= new FormControl('', [Validators.required]);
+
+  fechaLimite = new FormControl('', [Validators.required]);
+
+  observaciones = new FormControl('');
+
+  accionMejoraForm = new FormGroup(
+    {
+      accion: this.accion,
+      actividad: this.actividad,
+      responsable: this.responsable,
+      estado:this.estado,
+      fechaLimite: this.fechaLimite,
+      observaciones: this.observaciones,
+
+    }
+  );
+  
+  accionMejora(){
+    console.log(this.accionMejoraForm.value)
+  }
+
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es'
+
+registerLocaleData(localEs);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MatOptionModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+    
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID,
+      useValue:'es'
+    }
+  ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
